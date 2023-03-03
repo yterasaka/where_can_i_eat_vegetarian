6b2517ea2d950005f421fac23b6c7f20a83798d0
yterasaka <yuki.terasaka@gmail.com>
2023-03-03 21:49:53 +0100

    Create: Link button in header

--- components/Layout.js
+++ components/Layout.js
@@ -1,30 +1,40 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./layout.module.css";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={30}
-          height={30}
-          className={styles.logo}
-        />
-        <p className={styles.title}>Where Can I Eat Vegetarian in</p>
-        <select className={styles.selecter} name="city">
-          <option value="Tokyo">Tokyo</option>
-          <option value="Yokohama">Yokohama</option>
-          <option value="Kyoto">Kyoto</option>
-          <option value="Osaka">Osaka</option>
-        </select>
-        <p>?</p>
+        <div className={styles.title}>
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={30}
+            height={30}
+            className={styles.logo}
+          />
+          <p className={styles.title}>Where Can I Eat Vegetarian in</p>
+          <select className={styles.selecter} name="city">
+            <option value="Tokyo">Tokyo</option>
+            <option value="Yokohama">Yokohama</option>
+            <option value="Nagoya">Nagoya</option>
+            <option value="Kyoto">Kyoto</option>
+            <option value="Osaka">Osaka</option>
+          </select>
+          <p>?</p>
+        </div>
+        <div className={styles.link}>
+          <Link href={"/"}>
+            <FiGithub className={styles.linkButton} />
+          </Link>
+          <Link href={"/"}>
+            <FiLinkedin className={styles.linkButton} />
+          </Link>
+        </div>
       </header>
       <main>{children}</main>
-      {/* <footer className={styles.footer}>
-        <p>GitHub</p>
-      </footer> */}
     </div>
   );
 };
