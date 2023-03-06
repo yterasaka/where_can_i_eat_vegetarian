a958bd6a6ede5c7474f0d2f6ba0b6f60637304e3
yterasaka <yuki.terasaka@gmail.com>
2023-03-06 14:28:13 +0100

    Create: View Google Map

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,57 @@
-import styles from "./map.module.css";
+import React, { useRef, useCallback } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+// import PlaceInfo from './GoogleMapPlaceinfo';
 
-const Map = () => {
-  return <div className={styles.body}></div>;
+// 地図の大きさを指定
+const containerStyle = {
+  width: "100%",
+  height: "calc(100svh - 60px)",
 };
+
+// テスト
+const center = {
+  lat: 35.710057714926265,
+  lng: 139.81071829999996,
+};
+
+// オプション
+const options = {
+  disableDefaultUI: true, // 衛星写真オプションをキャンセル
+  zoomControl: true,
+};
+
+function Map() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+  });
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const renderMap = () => {
+    return (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={15}
+        options={options}
+        onLoad={onMapLoad}
+      >
+        {
+          // ...Your map components
+          // <PlaceInfo />
+        }
+      </GoogleMap>
+    );
+  };
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>;
+  }
+
+  return isLoaded ? renderMap() : <></>;
+}
 
 export default Map;
